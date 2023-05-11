2a9b2150f76f5886c14e6e068ad7b10492523e2f
Le Tuan Minh <letuanminh1892001@gmail.com>
2023-05-12 01:25:59 +0700

    update

--- app/src/main/java/com/example/photo_app/api/ApiClient.java
+++ app/src/main/java/com/example/photo_app/api/ApiClient.java
@@ -8,7 +8,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
+    //    private static final String BASE_URL = "https://jsqedvglu56wklwlbxfhvc2ude.srv.us/api/";
     private static final String BASE_URL = "https://4x6dhdewqcxwk32ykeseerrmda.srv.us/api/";
+
     private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create());
